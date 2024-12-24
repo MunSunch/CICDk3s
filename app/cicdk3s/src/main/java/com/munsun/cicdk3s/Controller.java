981f6995c631853cafb3624b35bed3b03936a410
MunSun <msunchalyaev@gmail.com>
2024-12-24 14:00:09 +0300

    change hello() in Controller

--- app/cicdk3s/src/main/java/com/munsun/cicdk3s/Controller.java
+++ app/cicdk3s/src/main/java/com/munsun/cicdk3s/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/")
     public String hello() {
-        return "Hello Munir!";
+        return "Hello Munir, Munir!";
     }
 }
